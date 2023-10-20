2c35ad37d14c0bd3948da0c37b85dcefa8edab84
Jax-Medlin <54479037+Jax-Medlin@users.noreply.github.com>
2023-10-19 20:18:31 -0500

    Update A.java

--- src/q1/extract_method/refactored/A.java
+++ src/q1/extract_method/refactored/A.java
@@ -1,27 +1,27 @@
-package q1.extract_method.refactored;
-
 import java.util.List;
 
 public class A {
    Node m1(List<Node> nodes, String p) {
-      // TODO: Your answer
-      // other implementation
+	   ex(nodes, p);
+	   // other implementation
       return null;
    }
 
-   Edge m2(List<Edge> edgeList, String p) {
-      // TODO: Your answer
-      // other implementation
+   Edge m2(List<Edge> edges, String p) {
+	   ex(edges, p);
+
+	   // other implementation
       return null;
    }
-
-   // TODO: Your answer
-   // ? extractedMethod(?) {
-   //
-   // }
+   <T extends Graph> void ex(List<T> objs, String p) {
+	   for(T obj : objs) {
+		   if(obj.contains(p))
+			   System.out.println(obj);
+	   }
+   }
 }
 
-class Node {
+class Node extends Graph {
    String name;
 
    public boolean contains(String p) {
@@ -29,10 +29,17 @@ class Node {
    }
 }
 
-class Edge {
+class Edge extends Graph{
    String name;
 
    public boolean contains(String p) {
        return name.contains(p);
    }
+}
+
+class Graph {
+	String name;
+	boolean contains(String p) {
+		return name.contains(p);
+	}
 }
